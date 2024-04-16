6ace144811752ec91f10237fdc3c247378200205
unknown <roxanvaz31@gmail.com>
2024-04-15 22:05:54 -0700

    attempted to connect to mongodb

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,21 @@
+import "dotenv/config"
+import mongoose from "mongoose"
 import express from 'express'
+
 // APP SETUP
 const app = express()
-const port = 5000
+const port = process.env.PORT
 
+// MONGOOSE CONNECTION & STARTING SERVER
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+.then(()=> {
+    console.log("Mongoose Connected")
+    app.listen(port, () => {
+    console.log("Server running on port:" + port)
+    })
+})
 // ENDPOINTS
 app.get("/", (req, res) => {
-    res.send("I'm Awake")
+    res.send("I'm Awake!")
 })
 
-// STARTING SERVER
-app.listen(port, () => {
-    console.log("Server running on port:" + port)
-})
